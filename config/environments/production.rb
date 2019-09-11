d5258ad08472d86bc1bc731c639da07fccc1c273
Olexandr Reshetnyk <olexandr.reshetnyk@gmail.com>
2019-09-11 18:23:24 +0300

    changed mailing settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 465,
       domain:               'gmail.com',
       user_name:            'dsgvjdfhhsfgvrtfsbvrgbrsf@gmail.com',
       password:             'qqq111```',
