6e0760e288b412deb3d99a82acd7060971b0b95e
Olexandr Reshetnyk <olexandr.reshetnyk@gmail.com>
2019-09-11 19:19:23 +0300

    changed mailing settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'dsgvjdfhhsfgvrtfsbvrgbrsf@gmail.com',
-      password:             'qqq111```',
-      authentication:       :plain,
-      enable_starttls_auto: true
-  }
+
 end
