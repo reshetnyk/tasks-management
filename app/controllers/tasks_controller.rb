f8956fdc28def236e7d8058cbb291474498e5a8c
Olexandr Reshetnyk <olexandr.reshetnyk@gmail.com>
2019-09-17 17:15:44 +0300

    CRUD and routing fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,35 +3,33 @@ class TasksController < ApplicationController
 
   def index
     @tasks = current_user.tasks
-    @status = params[:status]
-    @sort = params[:sort]
 
-    if @status
-      if @status.downcase == 'active'
+    if params.include? :status
+      if params[:status].downcase == 'active'
         @tasks = @tasks.where(is_completed: false)
       end
 
-      if @status.downcase == 'completed'
+      if params[:status].downcase == 'completed'
         @tasks = @tasks.where(is_completed: true)
       end
     end
 
-    if @sort
-      if @sort.downcase == 'asc'
+    if params.include? :sort
+      if params[:sort].downcase == 'asc'
         @tasks = @tasks.order(Task.arel_table['title'])
       end
 
-      if @sort.downcase == 'desc'
+      if params[:sort].downcase == 'desc'
         @tasks = @tasks.order(Task.arel_table['title'].desc)
       end
     end
   end
 
   def create
-    if params[:task]
+    if params.include? :task
       @task = current_user.tasks.build(task_params)
       if @task.save
-        redirect_to @task
+        redirect_to @task, notice: "Task was created."
       else
         render 'tasks/create'
       end
@@ -42,68 +40,55 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
-    if_someones_elses_task(@task)
-  end
-
-  def destroy_multiple
-    if params.include? :tasks_to_delete
-      Task.destroy(params[:tasks_to_delete])
+    begin
+      @task = current_user.tasks.find(params[:id])
+      if params.include? :act
+        if params[:act] == 'update'
+          render 'tasks/update'
+        end
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      @error = 'There is no such a task with \'id\'=\'' + params[:id] + '\'.'
+      render '/tasks/error'
     end
-
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path) }
-      format.json { head :no_content }
-    end
-  end
-
-  def edit
-    @task = Task.find(params[:id])
-    if_someones_elses_task(@task)
   end
 
   def update
-    @task = Task.find(params[:id])
-    if_someones_elses_task(@task)
-
-    if @task.update(task_params)
-      redirect_to @task, alert: "The task was successfully updated."
+    if params.include? :act
+      if params[:act] == 'complete'
+        @task = current_user.tasks.find(params[:id])
+        @task.update_attribute(:is_completed, !@task.is_completed)
+        @act = params[:act]
+        if params.include? :animate
+          @animate = params[:animate]
+        end
+        respond_to do |format|
+          format.js
+        end
+      end
     else
-      render 'edit'
+      @task = current_user.tasks.find(params[:id])
+      if @task.update(task_params)
+        redirect_to @task, notice: "The task was successfully updated."
+      else
+        render 'tasks/update'
+      end
     end
-
   end
 
   def destroy
-    @task = Task.find(params[:id])
-    if_someones_elses_task(@task)
-    @task.destroy
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path) }
-      format.json { head :no_content }
+    if params.include? :tasks_to_delete
+      Task.destroy(params[:tasks_to_delete])
+      flash[:error] = 'Tasks were deleted.'
+      redirect_back fallback_location: root_path
+    else
+      @task = current_user.tasks.find(params[:id])
+      @task.destroy
+      flash[:error] = 'Task was deleted.'
+      redirect_to '/tasks'
     end
   end
 
-  def complete
-    @task = Task.find(params[:id])
-    if_someones_elses_task(@task)
-    @task.update_attribute(:is_completed, !@task.is_completed)
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def access_denied
-  end
-
-
-
-
-  private def if_someones_elses_task(task)
-    if task.user_id != current_user.id
-      render 'access_denied'
-    end
-  end
   private def task_params
     params.require(:task).permit(:title, :description, :priority, :due_date)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :tasks, except: [:new] do
-    put 'complete', on: :member
-    get 'access_denied', on: :member
-    delete 'destroy_multiple', on: :collection
+  #
+  resources :tasks, except: [:new, :edit] do
     get 'create', on: :collection, as: 'create'
-    post 'create', on: :collection, as: 'post_create'
+    delete 'destroy', on: :collection, as: 'destroy'
   end
   get '', to: redirect('/tasks')
+  root to: 'tasks#index'
 end
